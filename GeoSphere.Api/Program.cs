cfec11bcc435116ef6e396e4b66a060a3f30d22a
Emir Duman <emirduman90@gmail.com>
2026-06-12 14:10:25 +0300

    Remove large texture source from git tracking

--- GeoSphere.Api/Program.cs
+++ GeoSphere.Api/Program.cs
@@ -32,7 +32,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors(FrontendCorsPolicy);
 
